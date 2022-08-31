14ee406f5429d763a3e2a80384de110caef76390
Pamela Fox <pamela.fox@gmail.com>
2022-08-31 10:58:46 -0700

    Rm dead code

--- image_filters.py
+++ image_filters.py
@@ -13,12 +13,9 @@ def remove_red(pixel_data):
 def remove_green(pixel_data):
     """Sets all green channels to 0 in a list of pixels."""
     new_pixel_data = copy.deepcopy(pixel_data)
-    # YOUR CODE HERE
     for row in new_pixel_data:
         for pixel in row:
             pixel[1] = 0
-    return new_pixel_data
-    # /YOUR CODE HERE
     return new_pixel_data
 
 
@@ -28,5 +25,4 @@ def remove_blue(pixel_data):
     for row in new_pixel_data:
         for pixel in row:
             pixel[2] = 0
-    return new_pixel_data
     return new_pixel_data
